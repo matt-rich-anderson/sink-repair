6b0968f46743251de2f8b98c75f52c166ddb8456
matt-rich-anderson <matt.rich.anderson@gmail.com>
2021-12-08 14:09:03 -0600

    button functionality - displays newest service request imediately

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -17,3 +17,22 @@ export const getRequests = () => {
     return applicationState.requests.map(request => ({...request}))
 }
 
+
+const mainContainer = document.querySelector("#container")
+
+export const sendRequest = (userServiceRequest) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userServiceRequest)
+    }
+
+
+    return fetch(`${API}/requests`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
+}
